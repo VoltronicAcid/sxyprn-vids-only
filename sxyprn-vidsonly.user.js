f1040e2a2d8cc90fe227bb066a39f023006084f8
VoltronicAcid <78665774+VoltronicAcid@users.noreply.github.com>
2025-06-16 10:02:01 -0400

    poc

---
+++ sxyprn-vidsonly.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        SxyPrn Vids Only
+// @description Automatically click the vids only checkbox
+// @author      VoltronicAcid
+// @version     0.1
+// @homepageURL https://github.com/VoltronicAcid/sxyprn-vids-only
+// @match       https://sxyprn.com/blog/all/*
+// @run-at      document-idle
+// @grant       GM.getValue
+// @grant       GM.setValue
+// @require     https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
+// ==/UserScript==
+
+const SETTING_KEY = "vidsOnly";
+
+(() => {
+    waitForKeyElements("label.vocontainer > span.checkmark", (checkbox) => {
+        console.clear();
+        console.log(checkbox);
+    });
+})();
