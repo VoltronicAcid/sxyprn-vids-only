4e9ed221934d9317b94b2c30a47134a2113889f6
VoltronicAcid <78665774+VoltronicAcid@users.noreply.github.com>
2025-07-12 08:10:54 -0400

    mutation observer

--- sxyprn-vidsonly.user.js
+++ sxyprn-vidsonly.user.js
@@ -2,32 +2,36 @@
 // @name        SexyPorn - Vids Only Setting
 // @description Automatically click the vids only checkbox
 // @author      VoltronicAcid
-// @version     1.0
+// @version     2.0
 // @homepageURL https://github.com/VoltronicAcid/sxyprn-vids-only
 // @supportURL  https://github.com/VoltronicAcid/sxyprn-vids-only/issues
 // @downloadURL https://github.com/VoltronicAcid/sxyprn-vids-only/raw/refs/heads/main/sxyprn-vidsonly.user.js
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=sxyprn.com
 // @match       https://sxyprn.com/blog/all/*
-// @run-at      document-idle
-// @grant       GM.getValue
-// @grant       GM.setValue
-// @require     https://cdn.jsdelivr.net/gh/CoeJoder/waitForKeyElements.js@v1.3/waitForKeyElements.js
 // ==/UserScript==
 
-const SETTING_KEY = "vidsOnly";
 
 (() => {
-    waitForKeyElements("label.vocontainer > input[type=checkbox]", (checkbox) => {
-        checkbox.addEventListener("change", () => {
-            GM.setValue(SETTING_KEY, checkbox.checked)
-                .catch((err) => console.error(err));
-        });
+    "use strict";
+    const settingKey = "vidsOnly";
+    const saveCheckboxState = (checkbox) => localStorage.setItem(settingKey, checkbox.checked);
 
-        GM.getValue(SETTING_KEY, true)
-            .then((vidSetting) => {
-                if (checkbox.checked !== vidSetting) {
-                    checkbox.click();
-                }
-            })
-            .catch((err) => console.error(err));
-    });
+    new MutationObserver((mutations, observer) => {
+        mutations.forEach(({ target }) => {
+            if (target.tagName === "HTML") {
+                const checkbox = target.querySelector("label.vocontainer > input[type=checkbox]");
+                const currSetting = localStorage.getItem(settingKey);
+
+                if (currSetting === null) saveCheckboxState(checkbox);
+
+                checkbox.addEventListener("change", () => {
+                    saveCheckboxState(checkbox);
+                });
+
+                if (currSetting && currSetting !== checkbox.checked.toString()) checkbox.click();
+
+                observer.disconnect();
+            }
+        });
+    }).observe(document, { childList: true, subtree: true, });
 })();
